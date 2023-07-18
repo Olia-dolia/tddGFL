9cec1a4ba6c97bbc8d5434fd5aa7bb4dd88c1090
Olia-dolia <dolechka4@gmail.com>
2023-07-18 20:04:17 +0300

    Final version

--- src/main/java/liakhoa/Main.java
+++ src/main/java/liakhoa/Main.java
@@ -2,15 +2,42 @@ package liakhoa;
 
 //variant 5
 
-import java.util.Arrays;
-import java.util.OptionalDouble;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 
 public class Main {
 
-    private final double A = 2.3;
-    private final double EPS = 1e-6;
+    private static final double A = 2.3;
+    private static final double EPS = 1e-6;
+    static double start = 0.2;
+    static double end = 2.8;
+    static double step = 0.002;
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.run();
+    }
+
+    private void run() {
+        double[] x = fillX(start, end, step);
+        double[] y = fillY(x);
+
+        printMinElement(getMinElement(x), minElement(x),"x");
+        printMaxElement(getMaxElement(x), maxElement(x),"x");
+        printMinElement(getMinElement(y), minElement(y),"y");
+        printMaxElement(getMaxElement(y), maxElement(y),"y");
+
+        double sumX = sum(x);
+        printSum(sumX, "x");
+        double sumY = sum(y);
+        printSum(sumY, "y");
+
+        double avgX = average(x);
+        printAvg(avgX, "x");
+        double avgY = average(y);
+        printAvg(avgY, "y");
+
+    }
 
     public double function(double x) {
         if (x <= 0.3 + EPS) {
@@ -23,11 +50,11 @@ public class Main {
         return Math.pow((x - 2), 2) * 6 * A;
     }
 
-    public int steps(double start, double end, double step) {
+    public static int steps(double start, double end, double step) {
         return (int) (Math.round((end - start) / step) + 1);
     }
 
-    public double[] fillX(double start, double end, double step) {
+    public static double[] fillX(double start, double end, double step) {
         return IntStream.range(0, steps(start, end, step)).mapToDouble(i -> start + i * step).toArray();
     }
 
@@ -35,8 +62,8 @@ public class Main {
         return DoubleStream.of(x).map(this::function).toArray();
     }
 
-    public int maxElement(double[] x) {
-        double element = DoubleStream.of(x).max().getAsDouble();
+    public static int maxElement(double[] x) {
+        double element = getMaxElement(x);
         int count = -1;
         for (double i : x) {
             count++;
@@ -45,8 +72,8 @@ public class Main {
         return 0;
     }
 
-    public int minElement(double[] x) {
-        double element = DoubleStream.of(x).min().getAsDouble();
+    public static int minElement(double[] x) {
+        double element = getMinElement(x);
         int count = -1;
         for (double i : x) {
             count++;
@@ -55,11 +82,35 @@ public class Main {
         return 0;
     }
 
-    public double sum(double[] x) {
+    public static double getMinElement(double[] x){
+        return DoubleStream.of(x).min().getAsDouble();
+    }
+
+    public static double getMaxElement(double[] x) {
+        return DoubleStream.of(x).max().getAsDouble();
+    }
+
+    public static double sum(double[] x) {
         return DoubleStream.of(x).sum();
     }
 
-    public double average(double[] x) {
+    public static double average(double[] x) {
         return DoubleStream.of(x).average().getAsDouble();
+    }
+
+    public static void printMinElement(double min, int index, String par) {
+        System.out.printf("Min element is: %f on position %d for %s \n", min, index, par);
+    }
+
+    public static void printMaxElement(double max, int index, String par) {
+        System.out.printf("Max element is: %f on position %d for %s \n", max, index, par);
+    }
+
+    public static void printSum(double sum, String par) {
+        System.out.printf("Sum is: %f for %s \n", sum, par);
+    }
+
+    public static void printAvg(double avg, String par) {
+        System.out.printf("Avg is: %f for %s \n", avg, par);
     }
 }
